211b0562eea60419b473435c3d81e8512363bbd7
Lucian Mihalca <infolucianmihalca@gmail.com>
2025-03-30 17:25:28 +0200

    feat(layout): implementación y ajustes de Header y Footer completados

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <main>
       <RouterView />
     </main>
-
+    <test />
     <footer-layout />
   </div>
 </template>
@@ -13,6 +13,7 @@
 <script setup lang="ts">
 import HeaderLayout from "./components/layout/HeaderLayout.vue";
 import FooterLayout from "./components/layout/FooterLayout.vue";
+// import Test from "./components/layout/TestLayout.vue";
 </script>
 
 <style lang="scss" scoped></style>
